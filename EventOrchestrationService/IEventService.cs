0803ab08d5739e5d6e9ac91f0121aa89d47bf844
Gimilim <79168787+Gimilim@users.noreply.github.com>
2026-03-31 22:32:37 +0300

    init proj
    Каркас контроллера

---
+++ EventOrchestrationService/IEventService.cs
@@ -0,0 +1,12 @@
+﻿using EventOrchestrationService.Models;
+
+namespace EventOrchestrationService;
+
+public interface IEventService
+{
+    List<Event> GetAllEvents();
+    Event? GetEventById(int id);
+    Event CreateEvent(Event newEvent);
+    Event? UpdateEvent(int id, Event updatedEvent);
+    bool DeleteEvent(int id);
+}
---
+++ EventOrchestrationService/Models/Event.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace EventOrchestrationService.Models;
+
+public class Event
+{
+    [Required(ErrorMessage = "ИД события обязателен для заполнения")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Название события обязательно для заполнения")]
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    [Required(ErrorMessage = "Дата начала события обязательно для заполнения")]
+    public DateTime StartAt { get; set; }
+
+    [Required(ErrorMessage = "Дата окончания события обязательно для заполнения")]
+    public DateTime EndAt { get; set; }
+
+    public class EventValidator : AbstractValidator<Event>
+    {
+        public EventValidator()
+        {
+            RuleFor(x => x.EndAt)
+                .GreaterThan(x => x.StartAt)
+                .WithMessage("Дата окончания должна быть больше даты начала");
+        }
+    }
+
+}
