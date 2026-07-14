31a6bbefbb49bc094ef19612fe0ec948be0b9989
Gimilim <79168787+Gimilim@users.noreply.github.com>
2026-07-14 21:13:16 +0300

    перенос в слой апи с кучей мелких правок в других слоях

--- EventOrchestrationService.API/Program.cs
+++ EventOrchestrationService.API/Program.cs
@@ -1,23 +1,53 @@
+using EventOrchestrationService.API.Middleware;
+using EventOrchestrationService.Application.Extensions;
+using EventOrchestrationService.Infrastructure.Data;
+using EventOrchestrationService.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(
+    builder.Configuration,
+    builder.Environment
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    try
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = app.Services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ошибка при применении миграций");
+        return;
+    }
+}
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
+namespace EventOrchestrationService.API
+{
+}
---
+++ EventOrchestrationService.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EventOrchestrationService.Application.Services;
+using EventOrchestrationService.Application.Interfaces;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventOrchestrationService.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IEventService, EventService>();
+        services.AddScoped<IBookingService, BookingService>();
+
+        services.AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
+
+        return services;
+    }
+}
