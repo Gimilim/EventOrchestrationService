b9ccda920120e46e2cc76317cee8feac659e644f
Gimilim <79168787+Gimilim@users.noreply.github.com>
2026-08-13 20:27:43 +0300

    перенесены конфигурации

---
+++ src/BookingService/BookingService.Infrastructure/Data/Configurations/BookingConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using BookingService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingService.Infrastructure.Data.Configurations;
+
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("Bookings", "public");
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Id)
+            .HasComment("ИД бронирования");
+
+        builder.Property(b => b.EventId)
+            .IsRequired()
+            .HasComment("ИД события");
+
+        builder.Property(b => b.UserId)
+            .IsRequired()
+            .HasComment("ИД пользователя");
+
+        builder.Property(b => b.Status)
+            .IsRequired()
+            .HasColumnType("smallint")
+            .HasConversion<short>()
+            .HasComment("""
+                        Статус бронирования:
+                        1 - В обработке (Pending)
+                        2 - Подтверждено (Confirmed)
+                        3 - Отклонено (Rejected)
+                        3 - Отменено (Cancelled)
+                        """);
+
+
+        builder.Property(b => b.CreatedAt)
+            .IsRequired()
+            .HasComment("Дата создания бронирования");
+
+        builder.Property(b => b.ProcessedAt)
+            .HasComment("Дата обработки события");
+    }
+}
---
+++ src/UserService/UserService.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserService.Domain.Entities;
+
+namespace UserService.Infrastructure.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users", "public");
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Id)
+            .HasComment("ИД пользователя");
+
+        builder.Property(u => u.Login)
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasComment("Логин пользователя");
+
+        builder.HasIndex(u => u.Login)
+            .IsUnique();
+
+        builder.Property(b => b.PasswordHash)
+            .IsRequired()
+            .HasComment("Хэш пароля");
+
+        builder.Property(b => b.Role)
+            .IsRequired()
+            .HasColumnType("smallint")
+            .HasConversion<short>()
+            .HasComment("""
+                        Роль Пользователя:
+                        1 - Пользователь (User)
+                        2 - Администратор (Admin)
+                        """);
+    }
+}
