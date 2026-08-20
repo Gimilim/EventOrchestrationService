a91c822a75d2f0f6229c3a0eb1d7e53b9e4c5e3f
Gimilim <79168787+Gimilim@users.noreply.github.com>
2026-08-21 01:28:20 +0300

    перенос букинг интеграционных тестов

---
+++ src/BookingService/BookingService.IntegrationTests/Fixtures/PostgreSqlContainerFixture.cs
@@ -0,0 +1,25 @@
+﻿using Testcontainers.PostgreSql;
+
+namespace BookingService.IntegrationTests.Fixtures;
+
+public class PostgreSqlContainerFixture : IAsyncLifetime
+{
+    public PostgreSqlContainer Container { get; } = new PostgreSqlBuilder("postgres:17-alpine")
+        .WithDatabase("testContainers")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await Container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Container.DisposeAsync();
+    }
+}
+
+[CollectionDefinition("Database collection")]
+public class DatabaseCollection : ICollectionFixture<PostgreSqlContainerFixture>
+{
+}
