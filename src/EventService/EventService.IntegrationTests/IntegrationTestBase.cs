ef691cf2bc06bef22fe83ee558da1e031cd1a4fd
Gimilim <79168787+Gimilim@users.noreply.github.com>
2026-08-21 01:34:07 +0300

    перенос эвент интеграционных тестов

---
+++ src/EventService/EventService.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,49 @@
+﻿using EventService.Infrastructure.Data;
+using EventService.IntegrationTests.Fixtures;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Testcontainers.PostgreSql;
+
+namespace EventService.IntegrationTests;
+
+[Collection("Database collection")]
+public abstract class IntegrationTestBase : IAsyncLifetime
+{
+    protected readonly PostgreSqlContainer Postgres;
+    private readonly DbContextOptions<AppDbContext> _options;
+
+    protected IntegrationTestBase(PostgreSqlContainerFixture fixture)
+    {
+        Postgres = fixture.Container;
+
+        _options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(Postgres.GetConnectionString())
+            .Options;
+    }
+
+    protected AppDbContext CreateContext()
+    {
+        var context = new AppDbContext(_options);
+        return context;
+    }
+
+    private async Task ResetDatabaseAsync()
+    {
+        NpgsqlConnection.ClearAllPools();
+        await using var context = CreateContext();
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.MigrateAsync();
+
+        await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE \"Events\" RESTART IDENTITY CASCADE;");
+    }
+
+    public async Task InitializeAsync()
+    {
+        await ResetDatabaseAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Task.CompletedTask;
+    }
+}
